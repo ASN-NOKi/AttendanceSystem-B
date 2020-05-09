9ef421231c95f2fc6f77aa5322089ddd7a3c78f2
Naoki Asano <brujita10n@gmail.com>
2020-05-09 06:33:57 +0000

    トップページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   
   root 'static_pages#top'
+  
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
