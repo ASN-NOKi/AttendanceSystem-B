fff2453c042fcf7143218868cc23bf7e0ce93f8c
Naoki Asano <brujita10n@gmail.com>
2020-05-09 16:54:16 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
+  has_secure_password
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -8,9 +11,41 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 100 }, 
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: true
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
+  
+  # 渡された文字列のハッシュ値を返す。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # トークンがダイジェストと一致すればtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+   # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_token, nil)
+  end
   
   
 end
